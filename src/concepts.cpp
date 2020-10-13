84bc4cdc03da8b5820ecb728977386e1afcc2795
William Coates <will.t.coates@gmail.com>
2020-10-13 01:52:30 +0100

    Added comparisons and constants to 'concepts'

--- src/concepts.cpp
+++ src/concepts.cpp
@@ -1,9 +1,16 @@
 #include "concepts.hpp"
 #include <unordered_map>
+#include <cmath>
 
 using namespace quickcalc;
 
 namespace {
+    constexpr double QC_FALSE = 0.0;
+    constexpr double QC_TRUE = 1.0;
+    constexpr double QC_EPSILON = 1e-15;
+    // C++17 doesn't define a pi constant, so we'll use our own
+    constexpr double QC_PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164063;
+    
     double qcIf(Executor &exec, const std::vector<ExprNode::ptr> &params) {
         if (params.size() < 2) {
             return NAN;
@@ -11,7 +18,7 @@ namespace {
         double param0 = exec.evaluate(params[0].get());
 
         // false
-        if (abs(param0) < 0.00001) {
+        if (abs(param0) < QC_EPSILON) {
             if (params.size() >= 3) {
                 return exec.evaluate(params[2].get());
             } else {
@@ -22,8 +29,78 @@ namespace {
         }
     }
 
+    double qcEq(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return abs(exec.evaluate(params[0].get()) - exec.evaluate(params[1].get())) < QC_EPSILON;
+    }
+
+    double qcNe(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return abs(exec.evaluate(params[0].get()) - exec.evaluate(params[1].get())) >= QC_EPSILON;
+    }
+
+    double qcGt(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return exec.evaluate(params[0].get()) > exec.evaluate(params[1].get());
+    }
+
+    double qcLt(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return exec.evaluate(params[0].get()) < exec.evaluate(params[1].get());
+    }
+
+    double qcGe(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return exec.evaluate(params[0].get()) >= exec.evaluate(params[1].get());
+    }
+
+    double qcLe(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        if (params.size() < 2) {
+            return NAN;
+        }
+        return exec.evaluate(params[0].get()) <= exec.evaluate(params[1].get());
+    }
+
+    double qcTrue(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        return QC_TRUE;
+    }
+    
+    double qcFalse(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        return QC_FALSE;
+    }
+
+    double qcEpsilon(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        return QC_EPSILON;
+    }
+
+    double qcPi(Executor &exec, const std::vector<ExprNode::ptr> &params) {
+        return QC_PI;
+    }
+
     std::unordered_map<std::string, ExecutorState::Func> functions = {
         { "if", &qcIf },
+        { "eq", &qcEq },
+        { "ne", &qcNe },
+        { "gt", &qcGt },
+        { "lt", &qcLt },
+        { "ge", &qcGe },
+        { "le", &qcLe },
+        { "TRUE", &qcTrue },
+        { "true", &qcTrue },
+        { "FALSE", &qcFalse },
+        { "false", &qcFalse },
+        { "EPSILON", &qcEpsilon },
+        { "PI", &qcPi },
     };
 }
 
